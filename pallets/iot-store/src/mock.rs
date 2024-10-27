35b7e2e514a637d4b8fa24136a4931e74b247d46
JyTey2004 <tey.jia.ye@gmail.com>
2024-10-27 12:31:48 +0800

    Made Initial Blockchain

---
+++ pallets/iot-store/src/mock.rs
@@ -0,0 +1,38 @@
+use crate as pallet_iot_store;
+use frame_support::derive_impl;
+use frame_support::pallet_prelude::ConstU32;
+
+use sp_runtime::BuildStorage;
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+    pub enum Test
+    {
+        System: frame_system,
+        IotStore: pallet_iot_store,
+    }
+);
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+    type Block = Block;
+}
+
+impl pallet_iot_store::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
+    type MaxVinLength = ConstU32<32>;
+    type MaxManufacturerLength = ConstU32<32>;
+    type MaxModelLength = ConstU32<32>;
+    type MaxMerkleRootLength = ConstU32<32>;
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
+        .unwrap()
+        .into()
+}
---
+++ pallets/iot-store/src/tests.rs
@@ -0,0 +1,11 @@
+use crate::mock::*;
+use frame_support::assert_ok;
+
+#[test]
+fn it_works_for_default_value() {
+    new_test_ext().execute_with(|| {
+        // Go past genesis block so events get deposited
+        System::set_block_number(1);
+        // Dispatch a signed extrinsic.
+    });
+}
